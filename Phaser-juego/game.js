56aec4e60a3f2970db5b29f805751f37324d121e
ExequielDani <exedaniel.09@gmail.com>
2025-03-16 01:38:17 -0300

    se agrega unos enemigos

--- Phaser-juego/game.js
+++ Phaser-juego/game.js
@@ -24,6 +24,11 @@ function preload() {
     this.load.image('oceano', 'https://labs.phaser.io/assets/skies/deepblue.png');
     this.load.image('buque', 'https://labs.phaser.io/assets/sprites/ufo.png');
     this.load.image('torpedo', 'https://labs.phaser.io/assets/sprites/bullet.png');
+    //enemigos
+    this.load.image('enemigoBuque', 'https://upload.wikimedia.org/wikipedia/commons/4/44/US_Navy_Destroyer.png');
+    this.load.image('enemigoSubmarino', 'https://upload.wikimedia.org/wikipedia/commons/6/6b/USS_Nautilus_SS-168.jpg');
+    this.load.image('torpedoEnemigo', 'https://labs.phaser.io/assets/sprites/bullet.png');
+
 }
 
 function create() {
@@ -39,6 +44,17 @@ function create() {
 
     // Capturar las teclas
     cursors = this.input.keyboard.createCursorKeys();
+
+    //grupo de enemigos
+    enemigos = this.physics.add.group();
+    torpedosEnemigos = this.physics.add.group();
+    // generar enemigos cada un tiempo
+    this.time.addEvent({
+    delay: 2000, //2 segundos
+    callback: crearEnemigo,
+    callbackScope: this,
+    loop: true
+    });
 
 }
 
@@ -67,7 +83,7 @@ function update() {
     //detecta al tocar espaico
     if (Phaser.Input.Keyboard.JustDown(cursors.space)) {
         dispararTorpedo();
-    }    
+    }
 }
 
 function dispararTorpedo() {
@@ -77,6 +93,37 @@ function dispararTorpedo() {
     torpedo.body.onWorldBounds = true;
 
     // Elimina torpedo al salir pantalla
+    torpedo.body.world.on('worldbounds', (body) => {
+        if (body.gameObject === torpedo) {
+            torpedo.destroy();
+        }
+    });
+}
+
+function crearEnemigo() {
+    let tipo = Phaser.Math.Between(0, 1); // 0 buque, 1 submarino
+    let x = Phaser.Math.Between(50, 750); // posicion aleatoria X
+    let enemigo = enemigos.create(x, 50, tipo === 0 ? 'enemigoBuque' : 'enemigoSubmarino');
+    
+    enemigo.setVelocityY(100); // movimiento enemigo abajo
+    enemigo.setCollideWorldBounds(true);
+
+    // disparo enemigo
+    this.time.addEvent({
+        delay: 3000, // 3 seg
+        callback: () => dispararTorpedoEnemigo(enemigo),
+        loop: true
+    });
+}
+
+function dispararTorpedoEnemigo(enemigo) {
+    if (!enemigo.active) return; // No dispara si se destruye
+
+    let torpedo = torpedosEnemigos.create(enemigo.x, enemigo.y, 'torpedoEnemigo');
+    torpedo.setVelocityY(300);
+    torpedo.setCollideWorldBounds(true);
+    //elim torpedos
+    torpedo.body.onWorldBounds = true;
     torpedo.body.world.on('worldbounds', (body) => {
         if (body.gameObject === torpedo) {
             torpedo.destroy();
